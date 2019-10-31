7bc9d36ca14e8ae67c87254c7659dc9b38e35b7d
Dinizoides <dinismatos6@gmail.com>
2019-10-31 17:30:12 +0000

    Função dividir_familias
    Adicionar a função dividir_familias ao The Modern Feud of the Capulets and Montagues

--- The Modern Feud of the Capulets and Montagues/main.py
+++ The Modern Feud of the Capulets and Montagues/main.py
@@ -1,5 +1,18 @@
 #Exercicio 4
 
+def dividir_familias (matriz):
+	#dividir a lista matriz por familias 
+	arr_F0 = []
+	arr_F1 = []
+	for i in matriz:
+		if i[2] == '0':
+			arr_F0.append(i)
+		else:
+			arr_F1.append(i)
+	#Teste : print (arr_F0)
+	#Teste : print (arr_F1)
+	return arr_F0, arr_F1
+
 
 
 matriz = matriz()
