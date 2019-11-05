48095c75eff14d957db5524fcba69a357eba7bd2
SarahSimonLuz <sarah@DESKTOP-HUKGJO9.localdomain>
2019-11-05 18:30:32 +0000

    Adicionar to_int

--- Administrative Reform/main.py
+++ Administrative Reform/main.py
@@ -15,6 +15,21 @@ def matriz():
 	#Teste: print (matriz, line1, line2)
 	return matriz, line1, line2 
 
+def to_int(matriz, line1, line2):
+	#Passar as strings da lista em ints
+	for i in matriz:
+		i[1] = int(i[1]) 
+		i[2] = int(i[2]) 
+		i[0] = int(i[0]) 
+
+	line1[0] = int(line1[0])
+	line1[1] = int(line1[1])
+	line2[0] = int(line2[0])
+	line2[1] = int(line2[1])
+
+	#Teste : print(matriz, line1, line2) 
+	return matriz, line1, line2
+
 
 matriz, line1, line2 = matriz()
 matriz, line1, line2 = to_int(matriz, line1, line2)
