7cb83bb60d450c71f46b1ac1196ee3ec306c1399
SarahSimonLuz <sarah@DESKTOP-HUKGJO9.localdomain>
2019-11-07 00:22:37 +0000

    Adicionar unit test

--- Greedy Trading/gdp.py
+++ Greedy Trading/gdp.py
@@ -1,24 +1,18 @@
-import statistics
-sequencia_atual = []
-sequencia_maior = []
 index_maior = 0
 count_u = 0
 out=[]
 escolhas = []
 
-def mediana (sequencia_maior):
-        #calcula a mediana da sequencia 
-        if len(sequencia_maior)%2!= 0:
-                return statistics.median(sequencia_maior)
-        else: 
-                sequencia_maior.sort()
-                tamanho = len(sequencia_maior)
-                return sequencia_maior[tamanho//2]
+def mediana(sequencia_maior):
+        #calcula a mediana da sequencia
+        sequencia_maior.sort()
+        tamanho = len(sequencia_maior)
+        return sequencia_maior[tamanho//2]
 
 def output(sequencia_maior, index_maior):
-    mediana_max = 0
-    mediana_max = mediana(sequencia_maior)
-    out.append([index_maior,count_u-1,mediana_max])
+        mediana_max = 0
+        mediana_max = mediana(sequencia_maior)
+        out.append([index_maior,count_u-1,mediana_max])
 
 def soma(sequencia_maior, sequencia_atual):
         soma_maior = 0
@@ -32,32 +26,45 @@ def soma(sequencia_maior, sequencia_atual):
         return 1
 
 def subsequencia(sequencia_maior, sequencia_atual):
-    soma_max = 0
-    soma_max = soma(sequencia_maior,sequencia_atual)
-    if soma_max==1:
-        for i in sequencia_atual:
-            sequencia_maior.append(i)
-        output(sequencia_maior, count_u - len(sequencia_maior))
-        return sequencia_maior
-    else:
-        sequencia_maior = sequencia_atual
-        index_maior = count_u - len(sequencia_maior)
-        output(sequencia_maior,index_maior )
-        return sequencia_maior
-    
-
-escolha = input()
-escolhas = escolha.split(' ')
-while escolhas[0]!= 'x':
-        if escolhas[0]=='u':
-                sequencia_atual.append(int(escolhas[1]))
-                count_u+=1
+        global count_u
+        soma_max = 0
+        soma_max = soma(sequencia_maior,sequencia_atual)
+        if soma_max==1:#a sequencia anterior continua a ser a 
+                for i in sequencia_atual:
+                    sequencia_maior.append(i)
+                output(sequencia_maior, count_u - len(sequencia_maior))
+                return sequencia_maior
         else:
-            sequencia_maior = subsequencia(sequencia_maior,sequencia_atual)
-            sequencia_atual = []
-        escolha = input()
-        escolhas = escolha.split(' ')
+                sequencia_maior = sequencia_atual
+                index_maior = count_u - len(sequencia_maior)
+                output(sequencia_maior,index_maior )
+                return sequencia_maior
 
-for i in out:
-    print(i[0],i[1],i[2])
 
+def ciclo(nome_ficheiro):
+        f = open(nome_ficheiro,"r")
+        escolhas = f.read().splitlines()
+        sequencia_atual = []
+        sequencia_maior = []
+        global count_u
+
+        for i in escolhas:
+                i = i.split(' ')
+                if i[0] == 'u':
+                        sequencia_atual.append(int(i[1]))
+                        count_u+=1
+                elif i[0] == 'q':
+                        sequencia_maior = subsequencia(sequencia_maior,sequencia_atual)
+                        sequencia_atual = []
+                else:
+                        break
+
+        for i in out:
+                print("{} {} {}".format(i[0],i[1],i[2]))
+
+        return out
+'''
+resultado = ciclo("input1.txt")
+for i in resultado:
+        print("{} {} {}".format(i[0],i[1],i[2]))
+'''
---
+++ Greedy Trading/test_gdp.py
@@ -0,0 +1,43 @@
+import unittest
+import StringIO
+import sys
+from gdp import mediana
+from gdp import output
+from gdp import soma
+from gdp import subsequencia
+from gdp import ciclo
+
+class TestGdp(unittest.TestCase):
+
+    def test_mediana(self):
+        resultado = mediana([1,2,3,4,5])
+        self.assertEqual(resultado,3)
+        
+        resultado = mediana([6,7])
+        self.assertEqual(resultado,7)
+        
+        resultado = mediana([8,9,10,11])
+        self.assertEqual(resultado,10)
+    
+    def test_soma(self):
+        resultado = soma([1,2,3,4,5],[1,2])
+        self.assertEqual(resultado,1)
+
+        resultado = soma([1,2],[1,2,3,4,5])
+        self.assertEqual(resultado,0)        
+
+    def test_subsequencia(self):
+        resultado = subsequencia([1,2,3,4,5],[1,2])
+        self.assertEqual(resultado,[1,1,2,2,3,4,5])
+  
+        resultado = subsequencia([1,2],[1,2,3,4,5])
+        self.assertEqual(resultado,[1,2,3,4,5])
+
+    def test_ciclo(self):
+        ret = ciclo("input1.txt")
+        self.assertEqual(ret,[[0,0,-5],[1,1,0],[2,4,2],[2,5,2]])
+
+        
+
+    
+
