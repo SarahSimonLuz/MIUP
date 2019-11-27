32f8e79d29bb55051770e8549887b584af993621
Dinizoides <dinismatos6@gmail.com>
2019-11-27 17:49:18 +0000

    Adicionar testes unitários ao RockMeNot
    Adiciona testes unitários ao RockMeNot

---
+++ RockMeNot/unit_teste.py
@@ -0,0 +1,33 @@
+import unittest
+from Rock import possivel
+from Rock import count
+from Rock import rock
+from Rock import matriz
+
+class Testrock(unittest.TestCase):
+        
+        def test_rock(self):
+                self.assertEqual(rock('teste1.txt'),True)
+                self.assertEqual(rock('teste2.txt'),True)
+
+        def test_possivel(self):
+                self.assertEqual(possivel((['.','.','.'],['.','.','.'],['.','.','.']),1,1,2,2),True)
+                self.assertEqual(possivel((['.','.'],['#','#']),0,0,1,1),False)
+
+        def test_count(self):
+                self.assertEqual(count((['.','.','.'],['.','.','.'],['.','.','.']),3,3),5)
+                self.assertEqual(
+
+
+                        count((['.','.'],['.','#']),2,2),1)
+
+        def test_matriz(self):
+                self.assertEqual(matriz(['3 3\n', '...\n', '...\n', '...']),([['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']], 3, 3))
+
+if __name__=='__main__':
+        unittest.main()
+
+
+
+
+
