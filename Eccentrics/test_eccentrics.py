29495487c06b1e36777ab51797eca22800765fc8
Ana Laura Barrias Ferro <l39872@alunos.uevora.pt>
2019-11-27 22:52:31 +0000

    Adicionar teste unitário

---
+++ Eccentrics/test_eccentrics.py
@@ -0,0 +1,45 @@
+import unittest
+import StringIO
+import sys
+#from eccentrics import print_camadas
+from eccentrics import mover_bolas
+
+class ElectionTests(unittest.TestCase):
+
+	#def test_print_camadas(self):
+	#	max_linhas = 6
+	#	camadas = [[9, -1, -1, -1, -1, -1],
+	#			   [1, -1, -1, -1, -1, -1],
+	#			   [8, 7, -1, -1, -1, -1],
+	#			   [5, -1, -1, -1, -1, -1],
+	#			   [2, 3, -1, -1, -1, -1],
+	#			   [4, 10, 11, -1, -1, -1]]
+#
+#		capturedOutput = StringIO.StringIO()
+#		sys.stdout = capturedOutput
+#		print_camadas(camadas, max_linhas)
+#		sys.stdout=sys.__stdout__
+#		print(capturedOutput.getvalue())
+
+	def test_mover_bolas(self):
+		n_camadas = 3
+		max_linhas = 6
+		camadas = [[9, -1, -1, -1, -1, -1],
+				   [1, -1, -1, -1, -1, -1],
+				   [8, 7, -1, -1, -1, -1],
+				   [5, -1, -1, -1, -1, -1],
+				   [2, 3, -1, -1, -1, -1],
+				   [4, 10, 11, -1, -1, -1]]
+		l = 0
+		c = 0
+		primeiro = 0
+
+		rslt_mover_bolas = mover_bolas(camadas, max_linhas, n_camadas, l, c, primeiro)
+		self.assertEqual(None, rslt_mover_bolas)
+
+if __name__=='__main__':
+        unittest.main()
+
+		
+
+
