6a5939d1901ba0a977579f82d8c8a449e7240e84
Ana Laura Barrias Ferro <l39872@alunos.uevora.pt>
2019-11-28 16:13:24 +0000

    Eccentrics updated

--- Eccentrics/eccentrics.py
+++ Eccentrics/eccentrics.py
@@ -1,9 +1,65 @@
 import sys
 import string
 
+def criar_camadas(max_linhas, n_camadas):
+	n_bolas_in = 0
+	max_peso = 0
+	i = 1
+	linha = 0
+
+	while i<=n_camadas:
+		j = 1
+
+		while j<=i:
+			k = 1
+
+			while k<=j:
+				n = raw_input()
+
+				if len(str(n)) > 1:
+					n = n.split(" ")
+					tamanho = len(n)
+					max_n = max(n)
+					max_peso = max(max_peso, max_n)
+
+					aux = 0
+					while aux<tamanho:
+						camadas[linha][aux] = int(n[aux])
+						aux = aux + 1
+
+					n_bolas_in = n_bolas_in + tamanho
+					k = k +tamanho
+
+				else:
+					max_peso = max(max_peso, n)
+					n_bolas_in = n_bolas_in + 1
+					k = k +1
+					camadas[linha][0] = int(n)
+
+				linha = linha + 1
+			j = j + 1
+		i = i +1
+
+	return camadas, n_bolas_in
+
+
+
+#def print_camadas(max_linhas, camadas):
+#	i = 0
+#	print("Camadas:")
+#	while i<max_linhas:
+#		j = 0
+#		for j in range(max_linhas):
+#			print(camadas[i][j]),
+#			j = j+1
+#
+#		i = i+1
+#		print("\n")
+
+
 def mover_bolas(camadas, max_linhas, n_camadas, l, c, primeiro):
 	if l >= max_linhas-n_camadas or l == -1:
-		return
+		return 
 
 	l_procura = l+1+primeiro
 	c_procura = c
@@ -35,3 +91,20 @@ def mover_bolas(camadas, max_linhas, n_camadas, l, c, primeiro):
 	#print_camadas(max_linhas, camadas)
 
 	mover_bolas(camadas, max_linhas, n_camadas, l_aux, c_aux, primeiro+1)
+
+
+
+n = 0
+n_bolas_out = 0
+
+n_camadas = input()
+max_linhas = (n_camadas *(n_camadas + 1))/2
+
+camadas = [[-1]*max_linhas for _ in range(max_linhas)]
+
+camadas, n_bolas_in = criar_camadas(max_linhas, n_camadas)
+
+while n_bolas_out != n_bolas_in:
+	print(camadas[0][0])
+	mover_bolas(camadas, max_linhas, n_camadas, 0, 0, 0)
+	n_bolas_out = n_bolas_out+1
