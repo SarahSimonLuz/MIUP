ef82dcfdbc042fbd9c1d4c3d04876550c3b1b732
Ana Laura Barrias Ferro <l39872@alunos.uevora.pt>
2019-12-18 13:45:49 +0000

    Adiciona func leitura()

--- Etapa 4/Problem Setter/problemSetter.py
+++ Etapa 4/Problem Setter/problemSetter.py
@@ -1,3 +1,8 @@
+def leitura(nome_ficheiro): #leitura do input
+    f = open(nome_ficheiro,"r")
+    escolhas = f.read().splitlines()
+    return escolhas 
+
 def decrementar(equipas): #decrementa o numero de emails por mandar, e remove da lista
 	for i in equipas:
 		i[0]-=1
