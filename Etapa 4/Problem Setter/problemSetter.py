e321374c5fecf0dceb9ae61048d16d814b2d1e4f
Ana Laura Barrias Ferro <l39872@alunos.uevora.pt>
2019-12-18 13:49:09 +0000

    Adiciona func remover_equipas

--- Etapa 4/Problem Setter/problemSetter.py
+++ Etapa 4/Problem Setter/problemSetter.py
@@ -7,3 +7,9 @@ def decrementar(equipas): #decrementa o numero de emails por mandar, e remove da
 	for i in equipas:
 		i[0]-=1
 	return equipas
+
+def remover_equipas(abertos, equipas): #remover as equipas que estão no abertos
+	for i in abertos:
+		if i in equipas:
+			equipas.remove(i)
+	return equipas
