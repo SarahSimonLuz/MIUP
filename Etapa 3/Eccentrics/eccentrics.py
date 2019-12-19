88edad861286fd13c9659d2964f419b07ea01f66
Ana Laura Barrias Ferro <l39872@alunos.uevora.pt>
2019-12-19 14:14:56 +0000

    Updated version

--- Etapa 3/Eccentrics/eccentrics.py
+++ Etapa 3/Eccentrics/eccentrics.py
@@ -1,61 +1,47 @@
 import sys
 import string
 
-def criar_camadas(max_linhas, n_camadas):
+def leitura(nome_ficheiro):
+	f = open(nome_ficheiro,"r")
+	ficheiro = f.read().splitlines()
+	f.close()
+	return ficheiro
+
+def criar_camadas(ficheiro, max_linhas, n_camadas):
 	n_bolas_in = 0
-	max_peso = 0
 	i = 1
 	linha = 0
+	camadas_aux = []
+	camadas = [[-1]*max_linhas for _ in range(max_linhas)]
 
 	while i<=n_camadas:
 		j = 1
-
 		while j<=i:
 			k = 1
-
 			while k<=j:
-				n = raw_input()
+				camadas_aux = ficheiro[linha].split(' ')
+				tamanho = len(camadas_aux)
 
-				if len(str(n)) > 1:
-					n = n.split(" ")
-					tamanho = len(n)
-					max_n = max(n)
-					max_peso = max(max_peso, max_n)
-
-					aux = 0
-					while aux<tamanho:
-						camadas[linha][aux] = int(n[aux])
-						aux = aux + 1
+				if tamanho > 1:
+					idx = 0
+					while idx<tamanho:
+						camadas[linha][idx] = int(camadas_aux[idx])
+						idx = idx + 1
 
 					n_bolas_in = n_bolas_in + tamanho
-					k = k +tamanho
+					k = k + tamanho
 
 				else:
-					max_peso = max(max_peso, n)
 					n_bolas_in = n_bolas_in + 1
 					k = k +1
-					camadas[linha][0] = int(n)
+					camadas[linha][0] = int(camadas_aux[0])
 
 				linha = linha + 1
 			j = j + 1
 		i = i +1
 
 	return camadas, n_bolas_in
-
-
-
-#def print_camadas(max_linhas, camadas):
-#	i = 0
-#	print("Camadas:")
-#	while i<max_linhas:
-#		j = 0
-#		for j in range(max_linhas):
-#			print(camadas[i][j]),
-#			j = j+1
-#
-#		i = i+1
-#		print("\n")
-
+'''
 def print_camadas(max_linhas, camadas):
 	i = 0
 	print("Camadas:")
@@ -68,7 +54,7 @@ def print_camadas(max_linhas, camadas):
 		i = i+1
 		print("\n")
 
-
+'''
 def mover_bolas(camadas, max_linhas, n_camadas, l, c, primeiro):
 	if l >= max_linhas-n_camadas or l == -1:
 		return 
@@ -105,18 +91,26 @@ def mover_bolas(camadas, max_linhas, n_camadas, l, c, primeiro):
 	mover_bolas(camadas, max_linhas, n_camadas, l_aux, c_aux, primeiro+1)
 
 
+def main(nome_ficheiro):
+	n = 0
+	n_bolas_out = 0
 
-n = 0
-n_bolas_out = 0
+	ficheiro_in = leitura(nome_ficheiro)
+	n_camadas = int(ficheiro_in.pop(0))
+	max_linhas = int((n_camadas *(n_camadas + 1))/2)
+	camadas, n_bolas_in = criar_camadas(ficheiro_in, max_linhas, n_camadas)
 
-n_camadas = input()
-max_linhas = (n_camadas *(n_camadas + 1))/2
+	ficheiro = open("output1.txt", "w")
+	   
+	while n_bolas_out != n_bolas_in:
+		ficheiro.write(str(camadas[0][0])+'\n')
+		mover_bolas(camadas, max_linhas, n_camadas, 0, 0, 0)
+		n_bolas_out = n_bolas_out+1
 
-camadas = [[-1]*max_linhas for _ in range(max_linhas)]
+	ficheiro.close()
+	ficheiro_out = leitura("output1.txt")
+	return ficheiro_out
 
-camadas, n_bolas_in = criar_camadas(max_linhas, n_camadas)
 
-while n_bolas_out != n_bolas_in:
-	print(camadas[0][0])
-	mover_bolas(camadas, max_linhas, n_camadas, 0, 0, 0)
-	n_bolas_out = n_bolas_out+1
+if __name__=='__main__':
+        main("input1.txt")
