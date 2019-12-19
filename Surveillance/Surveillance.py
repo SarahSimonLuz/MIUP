79b7b1d00c1a2575bf9cd3725ada5b0d1a90e15c
Dinizoides <dinismatos6@gmail.com>
2019-12-19 11:31:10 +0000

    Adicionar calcula_areaponto e arrayduplo ao Surveillance
    Adiciona calcula_areaponto e arrayduplo ao Surveillance

--- Surveillance/Surveillance.py
+++ Surveillance/Surveillance.py
@@ -24,6 +24,46 @@ def verifica(x):
     return True
 
 
+def calcula_areaponto(pontos,pontos2,size):
+    import numpy as np
+    from shapely.geometry import Point, Polygon
+    
+    countp = 0
+    poly = Polygon(pontos)
+    area = 0
+    areaponto = []
+    for i in range(size):
+        armazena = np.array(pontos2[i])
+        pcertos = 0
+    
+        for x in range(size-1):
+            inside = 0
+            countp=0
+
+            for y in range(size):
+                if verifica(resolve_matriz(pontos2[i],pontos2[i+x+1],pontos2[y],pontos2[y+1])) == True:
+                    countp+=1
+            if countp <= 4:
+                p1x = (pontos2[i][0] + pontos2[i+x+1][0])/2
+                p1y = (pontos2[i][1] + pontos2[i+x+1][1])/2
+                p1 = Point(p1x,p1y)
+            
+                if p1.within(poly) or p1.intersects(poly):
+                    armazena = np.append(armazena, pontos2[i+x+1])
+                
+        if recebecalcula(armazena,(len(armazena)-1)) == area:
+            areaponto.append(i)
+        if recebecalcula(armazena,(len(armazena)-1)) > area:
+            area = recebecalcula(armazena,(len(armazena)-1))
+            areaponto = [i]
+    return areaponto
 
 
+
+def arrayduplo(pontos):
+    pontos2 = []
+    for i in range(2):
+        for i in range(len(pontos)):
+            pontos2.append(pontos[i])
+    return pontos2
 
